d17fa9165150f369262bce5cbdc7a0d0591db32c
Alex <96791292+alex-1699@users.noreply.github.com>
2023-01-08 15:06:28 -0500

    Project setup
    The project exists now, I made a file structure and a Constants.java

---
+++ robocats2023/src/main/java/frc/team1699/Constants.java
@@ -0,0 +1,18 @@
+package frc.team1699;
+
+/**
+ * Constants are a bunch of public static final
+ * variables of the primitive data types.
+ * 
+ * They should be used for constants that affect
+ * all of the robot code, such as ports for actuators,
+ * sensors, or controllers.
+ * 
+ * They should be in camel case and start with a "k".
+ * An example would be:
+ * public static final int kJoystickPort = 0;
+ */
+
+public class Constants {
+    
+}
